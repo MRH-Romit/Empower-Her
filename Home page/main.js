7623e3d1662fa1e74b3b4b5903ac7c521817fd39
MRH-Romit <mdromit2002@gmail.com>
2024-07-05 02:49:04 +0600

    trying to do something

---
+++ Home page/main.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.querySelector('.chat-button').addEventListener('click', () => {
+    alert('Chat with ArthaSathi is not available yet.');
+});
+
+document.querySelector('.try-free-button').addEventListener('click', () => {
+    alert('Try it Free feature is not available yet.');
+});
