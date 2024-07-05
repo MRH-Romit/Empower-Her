903096403118843a071977ba16eeab94460036da
MRH-Romit <mdromit2002@gmail.com>
2024-07-05 22:25:13 +0600

    added half completed landing page

--- Home page/main.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-document.querySelector('.chat-button').addEventListener('click', () => {
-    alert('Chat with ArthaSathi is not available yet.');
-});
-
-document.querySelector('.try-free-button').addEventListener('click', () => {
-    alert('Try it Free feature is not available yet.');
-});
